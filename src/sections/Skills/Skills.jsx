1a8dc4dacb48bac3428f8c4274a0a060c22b735e
Koki Adachi <kadachi.mys@gmail.com>
2025-08-03 12:18:50 +0800

    checkIcon for dark mode added

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-light.svg";
+import checkLight from "../../assets/checkmark-light.svg";
+import checkDark from "../../assets/checkmark-dark.svg";
 import SkillList from "../../common/SkillList";
+import { useTheme } from "../../common/ThemeContext";
 
 function Skills() {
+  const { theme } = useTheme();
+  const checkIcon = theme === "light" ? checkLight : checkDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JacaScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkIcon} skill="HTML" />
+        <SkillList src={checkIcon} skill="CSS" />
+        <SkillList src={checkIcon} skill="JacaScript" />
+        <SkillList src={checkIcon} skill="Node" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkIcon} skill="React" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkIcon} skill="Redux" />
+        <SkillList src={checkIcon} skill="Git" />
+        <SkillList src={checkIcon} skill="Bootstrap" />
       </div>
     </section>
   );
