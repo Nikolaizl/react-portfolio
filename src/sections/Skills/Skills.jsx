beb54c9fe0ba89d71e7e21350fc969102f6b79ec
Koki Adachi <kadachi.mys@gmail.com>
2025-08-03 12:34:21 +0800

    updated skills setup

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,20 +13,37 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkIcon} skill="HTML" />
-        <SkillList src={checkIcon} skill="CSS" />
-        <SkillList src={checkIcon} skill="JacaScript" />
-        <SkillList src={checkIcon} skill="Node" />
-      </div>
-      <hr />
-      <div className={styles.skillList}>
+        <SkillList src={checkIcon} skill="HTML5" />
+        <SkillList src={checkIcon} skill="CSS3" />
+        <SkillList src={checkIcon} skill="JavaScript (ES6+)" />
         <SkillList src={checkIcon} skill="React" />
+        <SkillList src={checkIcon} skill="React Router" />
+        <SkillList src={checkIcon} skill="Redux" />
+        <SkillList src={checkIcon} skill="Bootstrap" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkIcon} skill="Redux" />
+        <SkillList src={checkIcon} skill="Express" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkIcon} skill="PostgreSQL" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkIcon} skill="Jest" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
         <SkillList src={checkIcon} skill="Git" />
-        <SkillList src={checkIcon} skill="Bootstrap" />
+        <SkillList src={checkIcon} skill="GitHub" />
+        <SkillList src={checkIcon} skill="CI/CD" />
+        <SkillList src={checkIcon} skill="Vercel" />
+        <SkillList src={checkIcon} skill="Firebase" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkIcon} skill="API Integration" />
       </div>
     </section>
   );
