888a1427faeb4cf6ec7ac6c5de2e14d219370890
Koki Adachi <kadachi.mys@gmail.com>
2025-08-04 13:55:08 +0800

    updated cv

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/koki_cv.pdf";
+import CV from "../../assets/koki.adachi_cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
