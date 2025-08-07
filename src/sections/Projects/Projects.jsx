e64f59f8f517fdc8c683c63944d91086ba9b5cfb
Koki Adachi <kadachi.mys@gmail.com>
2025-08-07 16:44:34 +0800

    added SHOPPER to projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import styles from "./ProjectsStyles.module.css";
 import funrun from "../../assets/funrun.png";
 import freshcut from "../../assets/freshcut.png";
 import good2eat from "../../assets/good2eat.png";
+import shopper from "../../assets/shopper.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -27,6 +28,12 @@ function Projects() {
           link="https://github.com/Nikolaizl/food-expiry-date-reminder"
           h3="Good2Eat"
           p="Expiry Date Reminder"
+        />
+        <ProjectCard
+          src={shopper}
+          link="https://github.com/Nikolaizl/frontend"
+          h3="SHOPPER"
+          p="Clothing E-commerce"
         />
       </div>
     </section>
